7a13b59859ccce3b06ac7b96de8844e8e523b04b
William C Grisaitis <wgrisaitis@gmail.com>
2020-05-14 23:39:01 -0400

    reinstall nvm

--- .profile.d/3_init_scripts.sh
+++ .profile.d/3_init_scripts.sh
@@ -1,10 +1,11 @@
+echo 3
+
 # rbenv
 eval "$(rbenv init -)"
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # pyenv
 if command -v pyenv 1>/dev/null 2>&1; then
