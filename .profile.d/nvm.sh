f211ddc7b5a53fd4096368ab171d916d531af2af
William C Grisaitis <wgrisaitis@gmail.com>
2020-05-27 12:26:57 -0400

    organize init commands into one file per service

--- .profile.d/nvm.sh
+++ .profile.d/nvm.sh
@@ -1,13 +1,7 @@
-# rbenv
-eval "$(rbenv init -)"
-
-# nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
 export PATH="$NVM_DIR/versions/node/$(ls $NVM_DIR/versions/node | sort -V | tail -n1)/bin:$PATH"
 alias nvm="unalias nvm; [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"; nvm $@"
 
-# pyenv
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
+# source for lazy loading:
+# https://github.com/nvm-sh/nvm/issues/1277#issuecomment-599019066
---
+++ .profile.d/poetry.sh
@@ -0,0 +1,2 @@
+export PATH="$HOME/.poetry/bin:$PATH"
+export VIRTUAL_ENV_DISABLE_PROMPT=1
