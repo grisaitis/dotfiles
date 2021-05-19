fc0c8880848885af93d026969a220786a5a2bdc1
William C Grisaitis <wgrisaitis@gmail.com>
2021-05-19 16:34:16 -0400

    update pyenv init

--- .profile.d/pyenv.sh
+++ .profile.d/pyenv.sh
@@ -2,6 +2,6 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
 if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init - --no-rehash)"
+  eval "$(pyenv init --path --no-rehash)"
   eval "$(pyenv virtualenv-init -)"
 fi
