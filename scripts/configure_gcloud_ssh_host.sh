fe8fbd0bb29f2951a0056081deae447f74208b24
William C Grisaitis <wgrisaitis@gmail.com>
2022-07-04 12:55:12 +0200

    change hostname pattern when setting jupyter as default user for ssh'ing to google cloud compute instances

--- scripts/configure_gcloud_ssh_host.sh
+++ scripts/configure_gcloud_ssh_host.sh
@@ -1,3 +1,3 @@
 gcloud compute config-ssh
 
-sed -i '' '/^Host notebook2.*/a\'$'\n''    User=jupyter'$'\n' ~/.ssh/config
+sed -i '' '/^Host .*keen-dispatch-316219.*/a\'$'\n''    User=jupyter'$'\n' ~/.ssh/config
